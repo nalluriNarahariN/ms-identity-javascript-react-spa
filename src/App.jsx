8ea338a0da73e0ff5d53b46504e159e76372146d
nalluriNarahari N <nallurinarahari.niranjan@cognizant.com>
2022-05-10 11:02:25 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,96 @@
 import React, { useState } from "react";
-import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import {
+  AuthenticatedTemplate,
+  UnauthenticatedTemplate,
+  useMsal,
+} from "@azure/msal-react";
 import { loginRequest } from "./authConfig";
 import { PageLayout } from "./components/PageLayout";
 import { ProfileData } from "./components/ProfileData";
 import { callMsGraph } from "./graph";
 import Button from "react-bootstrap/Button";
 import "./styles/App.css";
+import axios from "axios";
 
 /**
  * Renders information about the signed-in user or a button to retrieve data about the user
  */
 const ProfileContent = () => {
-    const { instance, accounts } = useMsal();
-    const [graphData, setGraphData] = useState(null);
+  const { instance, accounts } = useMsal();
+  const [graphData, setGraphData] = useState(null);
+  const [otp, setOtp] = useState("");
+  const [userData, setUserData] = useState({});
 
-    function RequestProfileData() {
-        // Silently acquires an access token which is then attached to a request for MS Graph data
-        instance.acquireTokenSilent({
-            ...loginRequest,
-            account: accounts[0]
-        }).then((response) => {
-            callMsGraph(response.accessToken).then(response => setGraphData(response));
+  const getOtp = (data) => {
+    axios
+      .post("http://localhost:3050/main/getOtp", data)
+      .then((res) => {
+        setOtp(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  function RequestProfileData() {
+    // Silently acquires an access token which is then attached to a request for MS Graph data
+    instance
+      .acquireTokenSilent({
+        ...loginRequest,
+        account: accounts[0],
+      })
+      .then((response) => {
+        callMsGraph(response.accessToken).then((res) => {
+          let data = {
+            firstName: res.givenName,
+            lastName: res.surname,
+            email: res.userPrincipalName,
+            jwtToken: response.accessToken,
+          };
+          setUserData(data);
+          setGraphData(res);
+          getOtp(data);
         });
-    }
+      });
+  }
 
-    return (
-        <>
-            <h5 className="card-title">Welcome {accounts[0].name}</h5>
-            {graphData ? 
-                <ProfileData graphData={graphData} />
-                :
-                <Button variant="secondary" onClick={RequestProfileData}>Request Profile Information</Button>
-            }
-        </>
-    );
+  return (
+    <>
+      <h5 className="card-title">Welcome {accounts[0].name}</h5>
+      {graphData ? (
+        <ProfileData otp={otp} graphData={graphData} />
+      ) : (
+        <Button variant="secondary" onClick={RequestProfileData}>
+          Request Profile Information
+        </Button>
+      )}
+    </>
+  );
 };
 
 /**
  * If a user is authenticated the ProfileContent component above is rendered. Otherwise a message indicating a user is not authenticated is rendered.
  */
-const MainContent = () => {    
-    return (
-        <div className="App">
-            <AuthenticatedTemplate>
-                <ProfileContent />
-            </AuthenticatedTemplate>
+const MainContent = () => {
+  return (
+    <div className="App">
+      <AuthenticatedTemplate>
+        <ProfileContent />
+      </AuthenticatedTemplate>
 
-            <UnauthenticatedTemplate>
-                <h5 className="card-title">Please sign-in to see your profile information.</h5>
-            </UnauthenticatedTemplate>
-        </div>
-    );
+      <UnauthenticatedTemplate>
+        <h5 className="card-title">
+          Please sign-in to see your profile information.
+        </h5>
+      </UnauthenticatedTemplate>
+    </div>
+  );
 };
 
 export default function App() {
-    return (
-        <PageLayout>
-            <MainContent />
-        </PageLayout>
-    );
+  return (
+    <PageLayout>
+      <MainContent />
+    </PageLayout>
+  );
 }
--- src/authConfig.js
+++ src/authConfig.js
@@ -6,53 +6,53 @@
 import { LogLevel } from "@azure/msal-browser";
 
 /**
- * Configuration object to be passed to MSAL instance on creation. 
+ * Configuration object to be passed to MSAL instance on creation.
  * For a full list of MSAL.js configuration parameters, visit:
- * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
  */
 export const msalConfig = {
-    auth: {
-        clientId: "Enter_the_Application_Id_Here",
-        authority: "Enter_the_Cloud_Instance_Id_HereEnter_the_Tenant_Info_Here",
-        redirectUri: "Enter_the_Redirect_Uri_Here"
+  auth: {
+    clientId: "1d19c423-7fc4-44e7-ae38-2f23ec654337",
+    authority: "https://login.microsoftonline.com/common",
+    redirectUri: "http://localhost:3000/redirect1",
+  },
+  cache: {
+    cacheLocation: "sessionStorage", // This configures where your cache will be stored
+    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        switch (level) {
+          case LogLevel.Error:
+            console.error(message);
+            return;
+          case LogLevel.Info:
+            console.info(message);
+            return;
+          case LogLevel.Verbose:
+            console.debug(message);
+            return;
+          case LogLevel.Warning:
+            console.warn(message);
+            return;
+        }
+      },
     },
-    cache: {
-        cacheLocation: "sessionStorage", // This configures where your cache will be stored
-        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
-    },
-    system: {	
-        loggerOptions: {	
-            loggerCallback: (level, message, containsPii) => {	
-                if (containsPii) {		
-                    return;		
-                }		
-                switch (level) {		
-                    case LogLevel.Error:		
-                        console.error(message);		
-                        return;		
-                    case LogLevel.Info:		
-                        console.info(message);		
-                        return;		
-                    case LogLevel.Verbose:		
-                        console.debug(message);		
-                        return;		
-                    case LogLevel.Warning:		
-                        console.warn(message);		
-                        return;		
-                }	
-            }	
-        }	
-    }
+  },
 };
 
 /**
  * Scopes you add here will be prompted for user consent during sign-in.
  * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
- * For more information about OIDC scopes, visit: 
+ * For more information about OIDC scopes, visit:
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
 export const loginRequest = {
-    scopes: ["User.Read"]
+  scopes: ["User.Read"],
 };
 
 /**
@@ -60,5 +60,5 @@ export const loginRequest = {
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
  */
 export const graphConfig = {
-    graphMeEndpoint: "Enter_the_Graph_Endpoint_Herev1.0/me"
+  graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
 };
